16797d625b99838b9602f30696e6598bc9b12f74
Dionata Ferraz <dionata.ferraz@ifood.com.br>
2023-08-30 19:19:48 -0300

    lesson-2: Adding run and apply

---
+++ app/src/main/java/br/com/fundatec/fundatecheroti22/HomeActivity.kt
@@ -0,0 +1,11 @@
+package br.com.fundatec.fundatecheroti22
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
