77a539c306974db95254092c446248aa69d053e1
Dionata Ferraz <dionata.ferraz@ifood.com.br>
2023-09-13 21:24:08 -0300

    feat: Adding Glide to load image and create a Toast and Snack bar message

--- app/src/main/java/br/com/fundatec/fundatecheroti22/home/view/HomeActivity.kt
+++ app/src/main/java/br/com/fundatec/fundatecheroti22/home/view/HomeActivity.kt
@@ -1,13 +1,16 @@
 package br.com.fundatec.fundatecheroti22.home.view
 
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import br.com.fundatec.fundatecheroti22.R
 import br.com.fundatec.fundatecheroti22.gone
 import br.com.fundatec.fundatecheroti22.home.presentation.model.HomeViewState
+import com.bumptech.glide.Glide
+import com.google.android.material.snackbar.Snackbar
 
 class HomeActivity : AppCompatActivity() {
     private val button by lazy {
@@ -21,10 +24,14 @@ class HomeActivity : AppCompatActivity() {
         observerState(HomeViewState.Error("Mensagem de erro"))
         Log.e("teste", "onCreate")
 
-        findViewById<Button>(R.id.button).setOnClickListener {
-            val intent = Intent(this@HomeActivity, MainActivity::class.java)
-            startActivity(intent)
-            finish()
+        val image: ImageView = findViewById(R.id.image)
+        Glide.with(this)
+            .load("https://cdn.kobo.com/book-images/ef771a94-af0c-4b43-9441-623fd4e2096f/353/569/90/False/venom-by-donny-cates-vol-5.jpg")
+            .into(image)
+
+        button.setOnClickListener {
+            Toast.makeText(this, R.string.app_name, Toast.LENGTH_SHORT).show()
+            Snackbar.make(button, R.string.app_name, Snackbar.LENGTH_LONG).show()
         }
     }
 
