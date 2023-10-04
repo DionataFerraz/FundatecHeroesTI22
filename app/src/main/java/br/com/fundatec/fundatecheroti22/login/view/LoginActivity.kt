61e3facb43e6887098d8d36e4dd430c619b88821
Dionata Ferraz <dionata.ferraz@ifood.com.br>
2023-10-04 19:26:15 -0300

    feat: Adding binding

--- app/src/main/java/br/com/fundatec/fundatecheroti22/login/view/LoginActivity.kt
+++ app/src/main/java/br/com/fundatec/fundatecheroti22/login/view/LoginActivity.kt
@@ -1,13 +1,38 @@
 package br.com.fundatec.fundatecheroti22.login.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import br.com.fundatec.fundatecheroti22.R
+import br.com.fundatec.fundatecheroti22.databinding.ActivityLoginBinding
+import br.com.fundatec.fundatecheroti22.home.view.HomeActivity
+import br.com.fundatec.fundatecheroti22.login.presentation.LoginViewModel
+import br.com.fundatec.fundatecheroti22.profile.view.ProfileActivity
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.Theme_FundatecHeroTI22)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        configButtonLogin()
+        configNewHereButton()
     }
+
+    private fun configButtonLogin() {
+        binding.btLogin.setOnClickListener {
+            startActivity(Intent(this@LoginActivity, HomeActivity::class.java))
+        }
+    }
+
+    private fun configNewHereButton() {
+        binding.tvNewHere.setOnClickListener {
+            startActivity(Intent(this@LoginActivity, ProfileActivity::class.java))
+        }
+    }
+
 }
---
+++ app/src/main/java/br/com/fundatec/fundatecheroti22/profile/view/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package br.com.fundatec.fundatecheroti22.profile.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.fundatec.fundatecheroti22.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
